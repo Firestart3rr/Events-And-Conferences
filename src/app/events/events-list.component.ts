98cb57f5a2025cac67097991a1db8ce498f6dc46
Firestart3rr <kuba.wziatka@gmail.com>
2020-05-08 13:54:19 +0200

    handling null values

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -9,8 +9,7 @@ import {Component} from '@angular/core';
       <hr>
       <div class="row">
         <div *ngFor="let event of events" class="col-md-5">
-          <event-thumbnail
-             [event]="event">
+          <event-thumbnail [event]="event">
           </event-thumbnail>
         </div>
       </div>
@@ -104,11 +103,7 @@ export class EventsListComponent {
       time: '9:00 am',
       price: 950.00,
       imageUrl: '/assets/images/ng-nl.png',
-      location: {
-        address: 'The NG-NL Convention Center & Scuba Shop',
-        city: 'Amsterdam',
-        country: 'Netherlands'
-      },
+      onlineUrl: 'http://ng-nl.org/',
       sessions: [
         {
           id: 1,
