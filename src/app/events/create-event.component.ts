aa3c352ca71cbae40268a596eca8c6b8c1353a1f
Firestart3rr <kuba.wziatka@gmail.com>
2020-05-13 20:22:23 +0200

    reversing the changes

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -15,26 +15,10 @@ import {EventService} from './shared';
 })
 export class CreateEventComponent {
 
-  newEvent: any;
+  newEvent
   isDirty = true
   constructor(private router: Router,
               private eventService: EventService) {
-  }
-
-  ngOnInit() {
-    this.newEvent = {
-      name: 'Ng Spectacular',
-      date: '8/8/8888',
-      time: '10am',
-      price: 799.99,
-      location: {
-        address: '456 Happy St',
-        city: 'Felicity',
-        country: 'Angularistan'
-      },
-      onlineUrl: 'http://ngSpectacular.com',
-      imageUrl: 'http://ngSpectacular.com/logo.png',
-    }
   }
 
   saveEvent(formValues) {
