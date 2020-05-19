5d8bbf41fd7a8532c5cb3d657c00ac736d2225c0
Firestart3rr <kuba.wziatka@gmail.com>
2020-05-19 12:42:27 +0200

    creating a custom validator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import {
   SessionListComponent,
   DurationPipe,
   UpvoteComponent,
-  VoterService
+  VoterService,
+  LocationValidator
 } from './events/index';
 
 import {EventsAppComponent} from './events-app.component';
@@ -50,7 +51,8 @@ let jQuery = window['$'];
     DurationPipe,
     SimpleModalComponent,
     ModalTriggerDirective,
-    UpvoteComponent
+    UpvoteComponent,
+    LocationValidator
   ],
   imports: [
     BrowserModule,
