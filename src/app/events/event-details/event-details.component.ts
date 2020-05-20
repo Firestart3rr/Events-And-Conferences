7308bc655b3b41b2a14242cfc830729e4bd27e34
Firestart3rr <kuba.wziatka@gmail.com>
2020-05-20 11:30:17 +0200

    listening to resolved data changes

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -32,12 +32,10 @@ export class EventDetailsComponent implements OnInit {
 
   ngOnInit() {
 
-    this.route.params.forEach((params: Params) => {
-      this.event = this.eventService.getEvent(+params['id']);
+    this.route.data.forEach((data) => {
+      this.event = data['event'];
       this.addMode = false;
     });
-
-    // this.event = this.eventService.getEvent(+this.route.snapshot.params.id);
   }
 
   addSession() {
