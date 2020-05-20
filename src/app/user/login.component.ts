e9e236099f435e7ce2b87340b938c3fdecb6ec62
Firestart3rr <kuba.wziatka@gmail.com>
2020-05-20 14:10:49 +0200

    integrating authentication with the server

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -10,17 +10,24 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent {
 
-  password
-  userName
-  mouseoverLogin
+  password;
+  userName;
+  mouseoverLogin;
+  loginInvalid = false;
 
   constructor(private authService: AuthService,
               private router: Router) {
   }
 
   login(formValues){
-    this.authService.loginUser(formValues.userName, formValues.password);
-    this.router.navigate(['/events']);
+    this.authService.loginUser(formValues.userName, formValues.password)
+      .subscribe(resp => {
+        if (!resp) {
+          this.loginInvalid = true;
+        } else {
+          this.router.navigate(['/events']);
+        }
+      });
   }
 
   cancel() {
