010e166e3f9e0fbad47987a04ad86308af011a2b
Firestart3rr <kuba.wziatka@gmail.com>
2020-05-20 13:22:33 +0200

    using QueryString parameters

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -27,35 +27,14 @@ export class EventService {
 
   saveEvent(event) {
     let options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
-    return this.http.post<IEvent>('/api/events', event, options)
+    return this.http.post<ISession[]>('/api/events', event, options)
       .pipe(catchError(this.handleError<IEvent>('saveEvent')));
 
   }
 
-  updateEvent(event) {
-    let index = EVENTS.findIndex(x => x.id = event.id);
-    EVENTS[index] = event;
-  }
-
-  searchSessions(searchTerm: string) {
-    var term = searchTerm.toLocaleLowerCase();
-    var results: ISession[] = [];
-
-    EVENTS.forEach(event => {
-      var matchingSessions = event.sessions.filter(session =>
-        session.name.toLocaleLowerCase().indexOf(term) > -1);
-      matchingSessions = matchingSessions.map((session: any) => {
-        session.eventId = event.id;
-        return session;
-      });
-      results = results.concat(matchingSessions);
-    });
-
-    var emitter = new EventEmitter(true);
-    setTimeout(() => {
-      emitter.emit(results);
-    }, 100);
-    return emitter;
+  searchSessions(searchTerm: string): Observable<ISession[]> {
+    return this.http.get<ISession[]>('/api/sessions/search?search=' + searchTerm)
+      .pipe(catchError(this.handleError<ISession[]>('searchSessions')));
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
