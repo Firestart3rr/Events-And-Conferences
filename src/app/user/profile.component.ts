983d78e68fb09dccba2eb7a1621ce897bf9bcca8
Firestart3rr <kuba.wziatka@gmail.com>
2020-05-20 15:51:29 +0200

    saving user data to the server

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -44,9 +44,10 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues) {
 
     if (this.profileForm.valid) {
-      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-      this.router.navigate(['events']);
-      this.toastr.success('Profile Saved');
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+        .subscribe(() => {
+          this.toastr.success('Profile Saved');
+        });
     }
   }
 
